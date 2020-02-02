f10c18d09c102caf7f4bd485111320a0b07b2e59
Vedant <vshetty.vs@gmail.com>
2020-02-02 22:48:05 +0530

    allow tab switching onclick

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,17 @@
+function openTab(evt, tabName) {
+  var i, content, tablinks;
+  content = document.getElementsByClassName("tab-content");
+
+  // Hide all content
+  for (i = 0; i < content.length; i++) {
+      content[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab");
+  
+  // Make all tabs non-active
+  for (i = 0; i < content.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace("is-active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " is-active";
+}
