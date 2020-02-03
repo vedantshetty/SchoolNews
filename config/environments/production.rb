a6a5e43ea2cc194b98848289430c2912b2f889c0
Vedant <vshetty.vs@gmail.com>
2020-02-03 14:47:17 +0530

    added devise and user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "blog_production"
 
+  # Devise 
+  config.action_mailer.default_url_options = { host: 'https://vedant-atlas.herokuapp.com/', port: 3000 }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
