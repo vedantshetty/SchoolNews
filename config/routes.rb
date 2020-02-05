c2b7fe0f6b221b8cb9e808a6fda3373a4c61ece3
Vedant <vshetty.vs@gmail.com>
2020-02-05 14:22:43 +0530

    added training posts

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ get '/service-worker.js', to: "service_worker#service_worker"
 get '/manifest.json', to: "service_worker#manifest"
 
   root 'posts#index'
-
+  
+#TEMP
 get '/innovation/3',:as=>'education', to: 'application#educationInnovation'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
