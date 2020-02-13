89e018752d28dded2b2a6b572a18b3739522ffe8
Vedant <vshetty.vs@gmail.com>
2020-02-13 16:13:49 +0530

    added a preloader

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,29 +1,4 @@
-window.openTab = function(evt, tabName) {
-  var i, content, tablinks;
-  content = document.getElementsByClassName("tab-content");
-
-  // Hide all content
-  for (i = 0; i < content.length; i++) {
-      content[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tab");
-  
-  // Make all tabs non-active
-  for (i = 0; i < content.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace("is-active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " is-active";
-}
-
-window.openModal = function() {
-
-  modal = this.document.getElementById('timepicker');
-  modal.className += ' is-active';
-}
-
-
-window.closeModal = function() {
-  modal = this.document.getElementById('timepicker');
-  modal.className = modal.className.replace(' is-active',"");
-}
+setTimeout(function(){
+  $(".preloader-wrapper").css("display",'none');
+  $('.content').css("display",'block');
+}, 2000);
