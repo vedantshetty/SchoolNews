b56929a10b6754e60613416fa38a8c0bc9c7479d
Vedant <vshetty.vs@gmail.com>
2020-02-21 19:38:37 +0530

    created fixed bottom nav

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -9,5 +9,12 @@ $(document).on('turbolinks:load', function(){
     $('.sidenav').sidenav({
     });
 
+    $('.carousel').carousel({
+      'dist': '-20',
+      'numVisible': 5
+    });
+
+    console.log($(window).width()/200)
+
     $('.fixed-action-btn').floatingActionButton();
 })
