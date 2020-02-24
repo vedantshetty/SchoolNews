526a4ce65137ceffc366c395c7c1da96e958289a
Vedant <vshetty.vs@gmail.com>
2020-02-24 18:42:17 +0530

    added code change to fix heroku action text bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
+require 'action_text'
 class ApplicationController < ActionController::Base
+  helper ActionText::Engine.helpers
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
